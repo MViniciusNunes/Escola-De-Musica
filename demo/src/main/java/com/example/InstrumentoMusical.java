4b5d31e4dadd2f5c5cc589b4a8bc3a3cda30a361
Marcos Vinicius Nunes Moreira <vinynunes32@gmail.com>
2025-10-07 10:24:48 -0300

    Atualização

---
+++ demo/src/main/java/com/example/InstrumentoMusical.java
@@ -0,0 +1,38 @@
+package com.example;
+
+public abstract class InstrumentoMusical {
+    protected  String nome;
+    protected  String material;
+    
+    public InstrumentoMusical (String nome, String material){
+        this.nome= nome;
+        this.material = material;
+    }
+
+    public abstract void tocar();
+
+    public void afinar(){
+        System.out.println("Afinando o " + nome + "...");
+    }
+
+    public void mostrarInformaçoes() {
+        System.out.println("Instrumento: " + nome);
+        System.out.println("Material " + material);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+}
---
+++ demo/src/main/java/com/example/Violao.java
@@ -0,0 +1,31 @@
+package com.example;
+
+public class Violao extends InstrumentoMusical {
+    private int numeroCordas;
+
+    public Violao (String nome, String meterial, int numeroCordas){
+        super(nome, material);
+        this.numeroCordas =numeroCordas;
+    }
+
+    @Override
+    public void tocar(){
+        System.out.println("Tocando notas suaves no " + super.getNome() + " com " + numeroCordas + " cordas...");
+        System.out.println("Dedilhando uma melodia romântica no violão");
+    }
+
+    @Override
+    public void afinar(){
+        super.afinar();
+        System.out.println("ajustando a tensão das" + numeroCordas+ "cordas");
+    } 
+
+    @Override
+    public void mostrarInformaçoes() {
+        super.mostrarInformaçoes();
+        System.out.println("Numeros de cordas: " + numeroCordas);
+
+    }
+
+    
+}
