1704fd49459d1bb36d5b29d68a10e16a7407a3df
Marcos Vinicius Nunes Moreira <vinynunes32@gmail.com>
2025-10-07 11:03:05 -0300

    EscolaMusical

---
+++ demo/src/main/java/com/example/EscolaMusical.java
@@ -0,0 +1,41 @@
+package com.example;
+
+
+public class EscolaMusical {
+    public static void main(String[] args) {
+
+        InstrumentoMusical[] banda = new InstrumentoMusical[4];
+
+        banda[0] = new Violao("Violão Clássico", "Madeira", 6);
+        banda[1] = new Piano("Piano de Cauda", "Madeira e Metal", 88);
+        banda[2] = new Bateria("Bateria Acústica", "Madeira e Metal", 5);
+        banda[3] = new Saxofone("Saxofone", "Latão", "Alto");
+
+        EscolaMusical escola = new EscolaMusical();
+        escola.apresentar(banda);
+    }
+
+    public void apresentar(InstrumentoMusical[] banda) {
+        System.out.println("=== A ORQUESTRA VAI COMEÇAR! ===");
+        System.out.println("--- Afinando os instrumentos ---");
+
+        // Afinar todos
+        for (InstrumentoMusical inst : banda) {
+            inst.afinar();
+        }
+
+        System.out.println("\n--- Instrumentos começaram a tocar ---");
+        for (InstrumentoMusical inst : banda) {
+            inst.tocar();
+        }
+
+        System.out.println("\n----- Mostrando informações dos Instrumentos -----");
+        for (int i = 0; i < banda.length; i++) {
+            System.out.println("\nInstrumento musical " + (i + 1) + " :");
+            banda[i].mostrarInformaçoes();
+        }
+
+        System.out.println("=================================================");
+        System.out.println("BUILD SUCCESS");
+    }
+}
